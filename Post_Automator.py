66a2e03a063e0eb0bfd0f6fa61889f4f90a5b4d7
Sasank423 <sasanksarma0@gmail.com>
2024-03-17 12:07:43 +0530

    Solved Errors

--- Post_Automator.py
+++ Post_Automator.py
@@ -10,8 +10,8 @@ from xlsxwriter import Workbook
 
 from PIL import Image
 from io import BytesIO
-import easyocr
 import sys
+import easyocr
 
 from base64 import b64decode
 import pandas as pd
@@ -43,11 +43,10 @@ def pdf_gen(pdfs, file_names=None):
 # Create a download button for the ZIP file
 
 
-def start(df,start_,end,sleep_):
+def start(df,start_,end):
     reader = easyocr.Reader(['en'])
     driver = webdriver.Chrome()
     driver.get("https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx")
-    
     def captcha_solve():
         link = driver.find_element(By.XPATH,"//div[@class = 'input-group']//img").get_attribute('src')
         response = requests.get(link)
@@ -79,74 +78,80 @@ def start(df,start_,end,sleep_):
             return cap[3] if len(cap) >= 4 else ''
         if context == 'Enter the Fifth number':
             return cap[4] if len(cap) == 5 else ''
-        if context == 'Evaluate the Expression':
-            try:
-                return eval(cap[:-1])
-            except :
-                return ''
-        return cap
+        
     
     pdfs = []
     
-    refs = list(df['RPAD Barcode No '])
     l = int(end)  if end != '' else len(refs)
     i = int(start_)  if start_ != '' else 1
-    sleep_ = int(sleep_) if sleep_ != '' or int(sleep_) > 4 else 4
     
     df = df[i-1:l]
     df.index = range(i,l+1)
     df_view = st.empty()
     df_view.dataframe(df)
+    cols = st.columns(4)
     with st.spinner('Please wait..'):
         sleep(1)
     with st.status("Processing.....",expanded=True):
-        while i<l:
-            if str(refs[i])=='nan':
-                i += 1
-                continue 
-            ip = driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_txtOrignlPgTranNo')
-            ip.clear()
-            ip.send_keys(refs[i])
-            cap = ''
-            c = 0
-            while cap=='':
-                cap = captcha_context()
-            driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_ucCaptcha1_txtCaptcha').send_keys(cap)
-            try:
-                driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_btnSearch').click()
-                sleep(sleep_)
-            except :
-                if c==5:
-                    df.loc[i,'Delivery Report'] = None
-                    df.loc[i,'date'] = None
-                    df.loc[i,'time'] = None
-                    df.loc[i,'office'] = None
-                    pdfs.append('')
+        try:
+            while i<=l:
+                ref = df.loc[i,'RPAD Barcode No ']
+                if str(ref)=='nan':
                     i += 1
-                    driver.get("https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx")
                     continue 
-                i += 1
+                ip = driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_txtOrignlPgTranNo')
+                ip.clear()
+                ip.send_keys(ref)
+                while 'number' not in driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_ucCaptcha1_lblCaptcha').text:
+                    driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_ucCaptcha1_imgbtnCaptcha').click()
+                    sleep(1)
+    #             c = 0
+                cap = ''
+                while cap=='':
+                    cap = captcha_context()
+                driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_ucCaptcha1_txtCaptcha').send_keys(cap)
+                try:
+                    driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_btnSearch').click()
+                except :
+                    if c==5:
+                        df.loc[i,'Delivery Report'] = None
+                        df.loc[i,'date'] = None
+                        df.loc[i,'time'] = None
+                        df.loc[i,'office'] = None
+                        pdfs.append('')
+                        i += 1
+                        driver.get("https://www.indiapost.gov.in/_layouts/15/DOP.Portal.Tracking/TrackConsignment.aspx")
+                        continue 
+                    i += 1
+                    sleep(1)
+                    c += 1
+                    continue
+                
+                while True:
+                    try:
+                        btn = driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_btnTrackMore')
+                        break
+                    except:
+                        pass
+                    
+                try:
+                    df.loc[i,'Delivery Report'] = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[4]").text)
+                    df.loc[i,'date']   = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[1]").text)
+                    df.loc[i,'time']  = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[2]").text)
+                    df.loc[i,'office'] = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[3]").text)
+                    btn.click()
+                    df_view.dataframe(df)
+    #                 pdfs.append(driver.execute_cdp_cmd('Page.printToPDF',{})['data'])
+                    st.write(str(i)+') Record '+ref+' is Completd.')
+                    i += 1
+                    count += 1
+                except:
+                    i-=1
+                i+=1   
                 sleep(1)
-                c += 1
-                continue
-            try:
-                btn = driver.find_element(By.ID,'ctl00_PlaceHolderMain_ucNewLegacyControl_btnTrackMore')                
-                df.loc[i,'Delivery Report'] = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[4]").text)
-                df.loc[i,'date']   = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[1]").text)
-                df.loc[i,'time']  = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[2]").text)
-                df.loc[i,'office'] = str(driver.find_element(By.XPATH,"//table[@class = 'responsivetable MailArticleEvntOER']//tbody//tr[2]//td[3]").text)
-                sleep(4)
-                btn.click()
-                df_view.dataframe(df)
-#                 pdfs.append(driver.execute_cdp_cmd('Page.printToPDF',{})['data'])
-                st.write(str(i)+') Record '+refs[i]+' is Completd.')
-                i += 1
-                count += 1
-            except:
-                i-=1
-            i+=1   
-            sleep(1)
-#     pdfs = pdf_gen(pdfs,refs)
+    #     pdfs = pdf_gen(pdfs,refs)
+    except:
+        pass
     return df
     
 
@@ -160,8 +165,7 @@ with cols[0]:
     start_ = st.text_input('Start at : ',placeholder='Index')
 with cols[1]:
     end = st.text_input('End at : ',placeholder='Index')
-with cols[2]:
-    sleep_ = st.text_input('Sleep Time : ',placeholder='Secounds')
+
 with cols[3]:
     st.write()
     st.write()
@@ -174,7 +178,7 @@ if bt:
         if len(list(df.columns)) != 6:
             st.error('ERROR!!! Invalid Excel Format')
         df.columns = ['Name','RPAD Barcode No ','date','time','office','Delivery Report']
-        df = start(df,start_,end,sleep_)
+        df = start(df,start_,end)
         excel_file = BytesIO()
         with pd.ExcelWriter(excel_file, engine='xlsxwriter', mode='w',) as writer:
             df.to_excel(writer, index=False)
